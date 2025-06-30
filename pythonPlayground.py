387176b57fbb322faccee9bc6464b50ae06ebab6
Skydermoby <ling1wen@gmail.com>
2025-06-30 14:26:25 -0400

    Added code to look for images

--- pythonPlayground.py
+++ pythonPlayground.py
@@ -174,9 +174,41 @@ if cycleMode:
         extraction(inputFile, outputFile)
 else:
     extraction(inputFile, outputFile)
+
+doc = pymupdf.open(inputFile)
+
+for page_index in range(len(doc)): # iterate over pdf pages
+    page = doc[page_index] # get the page
+    image_list = page.get_images(True)
+
+    # print the number of images found on the page
+    if image_list:
+        print(f"Found {len(image_list)} images on page {page_index}")
+    else:
+        print("No images found on page", page_index)
+
+    for image_index, img in enumerate(image_list, start=1): # enumerate the image list
+        xref = img[0] # get the XREF of the image
+        pix = pymupdf.Pixmap(doc, xref) # create a Pixmap
+
+        if pix.n - pix.alpha > 3: # CMYK: convert to RGB first
+            pix = pymupdf.Pixmap(pymupdf.csRGB, pix)
+
+        pix.save("page_%s-image_%s.png" % (page_index, image_index)) # save the image as png
+        pix = None
 #page1 = doc[1].get_textpage()
 
 #print(page1.extractTEXT())
 #print(doc.get_toc())
 #for page in doc:
     #print(page.get_text())
+
+from pypdf import PdfReader
+
+reader = PdfReader(inputFile)
+
+page = reader.pages[50]
+
+for count, image_file_object in enumerate(page.images):
+    with open(str(count) + image_file_object.name, "wb") as fp:
+        fp.write(image_file_object.data)
