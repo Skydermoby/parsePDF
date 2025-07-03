1c5c19d6a6386c7180d64f5d6a32459204cec628
Skydermoby <ling1wen@gmail.com>
2025-07-03 18:01:51 -0400

    Added local storage ability

--- testAPI.py
+++ testAPI.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, File, UploadFile
 from converter import extraction
+import os
+import shutil
 
 app = FastAPI()
 
@@ -9,8 +11,15 @@ async def create_upload_file(file: UploadFile | None = None):
     if not file:
         return {"message": "No upload file recieved"}
     else:
-        return extraction(file.file)
-        #return {"filename": file.filename}
+        outputFile = "Uploaded\\" + file.filename
+        if os.path.exists(outputFile):
+            os.remove(outputFile)
+        
+        with open(outputFile, "wb") as f:
+            f.write(file.file.read())
+        #shutil.copyfileobj(file.file, outputFile)
+        #return extraction(file.file)
+        return {"filename": file.filename}
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: str, q: str | None = None, short: bool = False):
