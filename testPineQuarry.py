4ba8a6dedff5a9be7cc17e0980d3796e86a9074e
Skydermoby <aaron.ye@edetek.com>
2025-07-09 17:20:58 -0400

    Update testPineQuarry.py

--- testPineQuarry.py
+++ testPineQuarry.py
@@ -5,3 +5,18 @@ pc = Pinecone(api_key="pcsk_6qzJGA_5xUfNgGmkyDar5tSg2gANqTCzPVWQjutiDaHyDDvFW8KE
 index_list = pc.list_indexes()
 
 print(index_list)
+
+#host name: aarontest-x2rea8e.svc.aped-4627-b74a.pinecone.io
+
+index = pc.Index(host="aarontest-x2rea8e.svc.aped-4627-b74a.pinecone.io")
+
+results = index.search(
+    namespace="example-namespace", 
+    query={
+        "inputs": {"text": "Disease prevention"}, 
+        "top_k": 2
+    },
+    fields=["category", "chunk_text"]
+)
+
+print(results)
