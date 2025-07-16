21d6cbebc5ed4c7c058e3b956e624b8c4ef2e2e6
Skydermoby <aaron.ye@edetek.com>
2025-07-16 17:57:35 -0400

    Added MUI for file conversion

--- helloworld/src/components/UploadFile.js
+++ helloworld/src/components/UploadFile.js
@@ -1,13 +1,33 @@
 import axios from "axios";
 import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+
+const VisuallyHiddenInput = styled('input')({
+  clip: 'rect(0 0 0 0)',
+  clipPath: 'inset(50%)',
+  height: 1,
+  overflow: 'hidden',
+  position: 'absolute',
+  bottom: 0,
+  left: 0,
+  whiteSpace: 'nowrap',
+  width: 1,
+});
+
 
 const UploadFile = () => {
 	const [selectedFile, setSelectedFile] = useState(null);
   const [savedData, setData] = useState(null);
   const [hasUpload, setUpload] = useState(false);
+  const [canDownload, setDownload] = useState(true);
+  const [canUpload, setCanUpload] = useState(true);
   const [message, setMessage] = useState("Please upload a pdf file to be converted into json")
 	const onFileChange = (event) => {
 		setSelectedFile(event.target.files[0]);
+		setDownload(true);
+		setCanUpload(false);
+		setMessage("Selected File: " + event.target.files[0].name);
 	};
 	const onFileUpload = () => {
 		const formData = new FormData();
@@ -21,6 +41,7 @@ const UploadFile = () => {
     .then(res => {
       console.log(res)
       setUpload(true)
+	  setCanUpload(true)
 	  setMessage("File successfully uploaded! Please wait for it to be converted")
     })
 	};
@@ -81,6 +102,7 @@ const UploadFile = () => {
       console.log(JSON.parse(res.data))
       setData(res.data)
 	  setMessage("File Conversion Completed! Please press the download button below!")
+	  setDownload(false)
     })
     setUpload(false);
   }
@@ -90,11 +112,23 @@ const UploadFile = () => {
 			<h1>Aaron's even shabbier front end 0_0</h1>
 			<h3>PDF to JSON Conversion:</h3>
 			<div>
-				<input type="file" onChange={onFileChange} /> 
-				<button onClick={onFileUpload}>Upload!</button>
+				<Button
+					component="label"
+					role={undefined}
+					variant="contained"
+					tabIndex={-1}
+					>
+					Upload files
+					<VisuallyHiddenInput
+						type="file"
+						onChange={onFileChange}
+						multiple
+					/>
+				</Button>
+				<Button onClick={onFileUpload} variant="contained" disabled={canUpload}>Upload!</Button>
 			</div>
 			<div>{message}</div>
-			<button onClick={onButtonClick}> Download JSON File </button>
+			<Button onClick={onButtonClick} variant="contained" disabled={canDownload}> Download JSON File </Button>
 		</div>
 		
 	);
