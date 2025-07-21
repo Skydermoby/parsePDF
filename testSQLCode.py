82d74210c256c74f6bbdbe37e3abd2cc643e5782
Skydermoby <aaron.ye@edetek.com>
2025-07-21 18:11:24 -0400

    minor testing changes

--- testSQLCode.py
+++ testSQLCode.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 #Upload example hero
-#files = {"id": 11, "name": "Fortnite", "age": 21, "secret_name": "Battlepass"}
-#print(files)
-#res = requests.post(url= "http://127.0.0.1:8000/heroes/", json=files)
+files = {"id": 999, "name": "Fortnite", "age": 21, "secret_name": "Battlepass"}
+print(files)
+res = requests.post(url= "http://127.0.0.1:8000/heroes/", json=files)
 
-res = requests.get(url= "http://127.0.0.1:8000/heroes/11")
+res = requests.get(url= "http://127.0.0.1:8000/heroes/")
 print(res.text)
